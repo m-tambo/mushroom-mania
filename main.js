9b5d17cefc89c5cc504c53d016170d9f6f5309bb
Michael Tambornino <michael.tambornino@gmail.com>
2017-01-19 15:37:56 -0600

    showed and filtered shrooms

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 angular
    .module('shrooms', ['ngRoute'])
-   .config((routeProvider) => {
+   .config(($routeProvider) => {
       $routeProvider
          .when('/', {
             controller: "mainCtrl",
             templateUrl: "/shrooms.html"
          })
    })
-   .controller('mainCtrl', function($scope, shroomFactory) => {
-      console.log(shroom control)
-      shroomFactory.getShrooms() => (console.log)
+   .controller('mainCtrl', function($scope, shroomFactory) {
+      console.log('schroom controller')
+      shroomFactory
+         .getShrooms()
+         .then((mushrooms) => {
+            $scope.shrooms = mushrooms
+            // $scope.description = mushrooms
+         })
 
    })
    .factory('shroomFactory', function ($http) {
@@ -19,6 +24,7 @@ angular
                .get('https://mushroom-mania-b4961.firebaseio.com/.json')
                .then( (response) => {
                   console.log(response)
+                  return response.data.mushrooms
                })
             }
       }
