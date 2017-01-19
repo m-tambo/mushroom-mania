ba448a845608a109b9a5f86f186184ac79695e70
Michael Tambornino <michael.tambornino@gmail.com>
2017-01-19 14:28:02 -0600

    building app structure

--- main.js
+++ main.js
@@ -7,3 +7,19 @@ angular
             templateUrl: "/shrooms.html"
          })
    })
+   .controller('mainCtrl', function($scope, shroomFactory) => {
+      console.log(shroom control)
+      shroomFactory.getShrooms() => (console.log)
+
+   })
+   .factory('shroomFactory', function ($http) {
+      return {
+         getShrooms : function () {
+            return $http
+               .get('https://mushroom-mania-b4961.firebaseio.com/.json')
+               .then( (response) => {
+                  console.log(response)
+               })
+            }
+      }
+   })
